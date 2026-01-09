64c1356424ad64fcda8118e0bad463cc357532a4
ariyan <ariyanattt@gmail.com>
2026-01-09 12:07:37 -0700

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TrafficyLandingPage.Models;
+
+namespace TrafficyLandingPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
+        public HomeController(IWebHostEnvironment env, ILogger<HomeController> logger)
+        {
+            _env = env;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult Serve(string id)
+        {
+            //Console.WriteLine(id);
+            switch (id)
+            {
+                case "personal_logo":
+                    return PhysicalFile(Path.Combine(_env.ContentRootPath, "/app/wwwroot/images/personal_logo.png"), "image/png");
+                case "tabsy_icon":
+                    return PhysicalFile(Path.Combine(_env.ContentRootPath, "/app/wwwroot/images/tabsyicon.png"), "image/png");
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrafficyLandingPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
