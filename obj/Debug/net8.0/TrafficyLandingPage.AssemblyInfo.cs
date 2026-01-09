96620dd36522b827a1b32961ee788b22a19807d8
ariyan <ariyanattt@gmail.com>
2026-01-09 12:33:17 -0700

    Added link to twitter account

--- obj/Debug/net8.0/TrafficyLandingPage.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrafficyLandingPage.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrafficyLandingPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c1356424ad64fcda8118e0bad463cc357532a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d3abfe1c862e1da3011fbd35ed481acf026c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrafficyLandingPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrafficyLandingPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
